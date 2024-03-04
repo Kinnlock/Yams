9c57b6abdbbc8d1d9338b8d3650461674ef0f6c5
anthony20021 <156093526+anthony20021@users.noreply.github.com>
2024-03-04 17:51:00 +0100

    récupération des gains fonctionnel

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,58 @@
-import { useGetPastriesQuery } from './storage/game'
-
+import { useGetPastriesQuery, useGetWinQuery } from './storage/game';
 
 function App() {
-  const { data, error, isLoading } = useGetPastriesQuery();
-  const pastries = data
+  const { data: pastries, error: pastriesError, isLoading: pastriesIsLoading } = useGetPastriesQuery();
 
-  if (isLoading) {
-    return <div>Loading...</div>;
+  if (pastriesIsLoading) {
+    return <div>Loading pastries...</div>;
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
+  if (pastriesError) {
+    return <div>Error loading pastries: {pastriesError.message}</div>;
   }
-  if (data) {
+
+  if (pastries) {
     return (
       <>
-        {data.map((pastrie) => (
-          <p key={pastrie.id}>{pastrie.name}</p>
-        ))}
+        <Game />
       </>
     );
   }
 }
 
-export default App
+//Faire une logique (lancer 5 fois dé et si y a des doubles ou trible alors on lance Game avec une prop 1 ou 2 qui serait le return de dé s'il y a respectivement un double ou un triple)
+function result(){
+
+}
+
+//Le dé qui donne un resultat entre 1 et 6
+function de(){
+  return Math.floor(Math.random() * (6-1) + 1);
+}
+
+
+//A mettre dans un fichier component jsx à part /!\
+function Game() {
+  const { data: wins, error: winError, isLoading: winIsLoading } = useGetWinQuery(1);
+
+  if (winIsLoading) {
+    return <div>Loading win...</div>;
+  }
+
+  if (winError) {
+    return <div>Error loading win: {winError.message}</div>;
+  }
+  if(wins){
+    console.log(wins)
+    return (
+      <>
+        <h1>Bravo vous avez gagné un(e)</h1>
+        {wins.map((win) => (
+          <p key={win.id}>{win.name}</p>
+        ))}
+     </>
+    )
+  }
+}
+
+export default App;
