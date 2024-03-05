8e3d13930e83a4aeb03d67425c3e2a97bdca78cd
anthony20021 <156093526+anthony20021@users.noreply.github.com>
2024-03-05 17:04:36 +0100

    pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,99 +1,18 @@
-import { useState } from 'react';
-import Prize from './components/Prize';
-import { useGetPastriesQuery } from './storage/game';
-import Button from './components/Button';
 import './App.css';
+import Game from './components/game';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const { data: pastries, error: pastriesError, isLoading: pastriesIsLoading } = useGetPastriesQuery();
-  const [resultat, setResultat] = useState([]); 
+  return (
+    <>
+      <Router>
+        <Routes>
+         <Route path="/game" element={<Game></Game>}></Route>
+        </Routes>
 
-  if (pastriesIsLoading) {
-    return <div>Chargement...</div>;
-  }
-
-  if (pastriesError) {
-    return <div>Erreur lors du chargement: {pastriesError.message}</div>;
-  }
-
-  if (pastries) {
-    const handleClick = () => {
-      const newResultat = result(); 
-      setResultat(newResultat); 
-    };
-
-    return (
-      <>
-        <div className='container'>
-
-          <div className='dices-container'>
-            <div className='dices'>
-            {resultat[0]}
-            </div>
-            <div className='dices'>
-            {resultat[1]}
-            </div>
-            <div className='dices'>
-            {resultat[2]}
-            </div>
-            <div className='dices'>
-            {resultat[3]}
-            </div>
-            <div className='dices'>
-            {resultat[4]}
-            </div>
-          </div>
-          {(resultat[5] < 1 || resultat.length<1) && (
-            <Button 
-              label="Lancer le dé"
-              onClick={() => handleClick()}
-              width="150px"
-              height="40px"
-              color="antiquewhite"
-              backgroundColor="#052E33"
-              borderRadius="15px"
-              fontSize="1em"
-              margin="25px"
-            />
-          )}
-        </div>
-        
-        {resultat[5] > 0 && <Prize quantity={resultat[5]} />} 
-      </>
-    );
-  }
-}
-
-function result() {
-  let resultats = [];
-  let gagner = 0;
-  
-  for (let i = 0; i < 5; i++) {
-    resultats.push(de());
-  }
-  
-  let counts = {}; 
-
-  resultats.forEach(nombre => {
-    counts[nombre] = (counts[nombre] || 0) + 1;
-  });
-
-
-  Object.values(counts).forEach(count => {
-    if (count === 3) {
-      gagner += 1; 
-    } else if (count === 4) {
-      gagner += 2; 
-    }
-  });
-
-  resultats.push(gagner);
-  return resultats;
-}
-
-//Le dé qui donne un resultat entre 1 et 6
-function de(){
-  return Math.floor(Math.random() * (6-1) + 1);
+      </Router>
+    </>
+  )
 }
 
 export default App;
