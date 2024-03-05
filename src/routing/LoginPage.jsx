72b88137b4509c71143735828683cd4e649e4537
Nacer Eddine Attar <attar.nacereddine@gmail.com>
2024-03-05 17:27:23 +0100

    loginpage

--- src/routing/LoginPage.jsx
+++ src/routing/LoginPage.jsx
@@ -1,1 +1,19 @@
 import "./LoginPage.css"
+import {useState} from "react"
+
+const LoginPage = () => {
+    const [state,setState] = useState("")
+    return (
+        <div className="LoginPage">
+            <h1>Login Page</h1>
+            <label for="email">Email</label>
+            <input id="email" type="email" ></input>
+            <label for="password">Mot de passe</label>
+            <input id="password" type="password" ></input>
+        </div>
+    )
+}
+
+
+
+export default LoginPage
