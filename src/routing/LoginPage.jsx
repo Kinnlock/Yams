3bab8fabc5c337d9318b45a8ee49805fb0a2d0d6
Nacer Eddine Attar <attar.nacereddine@gmail.com>
2024-03-06 10:35:07 +0100

    login error

--- src/routing/LoginPage.jsx
+++ src/routing/LoginPage.jsx
@@ -1,19 +1,47 @@
 import "./LoginPage.css"
 import {useState} from "react"
+import Button from "../components/Button";
+import { usePostLoginQuery } from "../storage/game";
+import React from "react";
 
 const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [state,setState] = useState("")
+
+    const handleChangeEmail = (e) => {
+        setEmail(e.target.value);
+      };
+    
+      const handleChangePassword = (e) => {
+        setPassword(e.target.value);
+      };
+    
+      const handleLogin = () => {
+        setState({ email: email, password: password });
+        const { data, loading, error } = usePostLoginQuery(state);
+        console.log(data) ;
+      };
+
+    //   const login  = (email,password) => {
+    //     setState({ email: email, password: password });
+    //     const { data, loading, error } = usePostLoginQuery(state);
+    //   };
+
+
+
     return (
         <div className="LoginPage">
             <h1>Login Page</h1>
-            <label for="email">Email</label>
+            <label for="email" onChange={handleChangeEmail}>Email</label>
             <input id="email" type="email" ></input>
-            <label for="password">Mot de passe</label>
+
+            <label for="password" onChange={handleChangePassword}>Mot de passe</label>
             <input id="password" type="password" ></input>
+
+            <Button onClick={handleLogin} label="Se connecter" />
         </div>
     )
 }
-
-
 
 export default LoginPage
