48b24f7b7bc9d5a03bb4a32d4e8e3b00ae5b178a
anthony20021 <156093526+anthony20021@users.noreply.github.com>
2024-03-08 14:05:24 +0100

    git push

--- src/components/Prize.jsx
+++ src/components/Prize.jsx
@@ -12,18 +12,26 @@ function Prize({ quantity }) {
   if (winError) {
     return <div>Error loading win: {winError.message}</div>;
   }
-  if (wins.length>0) {
+
+  if (wins.length > 0) {
     return (
       <>
         <h1 className="title">Bravo, vous avez gagné un(e) :</h1>
         {[...wins].map((win) => (
-          <p key={win.id} className="pastries-name">{win.name}</p>
+          <div key={win.id}>
+            <p className="pastries-name">{win.name}</p>
+            {win.image !== "http://placehold.it/32x32" && (
+              <img className="table-image" src={"http://localhost:3001/uploads/images/" + win.image} alt={win.name} />
+            )}
+            {win.image === "http://placehold.it/32x32" && (
+              <img className="table-image" src={win.image} alt={win.name} />
+            )}
+          </div>
         ))}
       </>
     );
-  }
-  else{
-    return <h1 className="title">Plus de pâtisseries disponnibles</h1>
+  } else {
+    return <h1 className="title">Plus de pâtisseries disponibles</h1>;
   }
 }
 
